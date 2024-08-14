d6464dc826d387982911f13fd224f60ca557cdda
AydanBayramova <bayramoffaaydan1@gmail.com>
2024-08-14 14:09:17 +0400

    ProfileService interface used for crud operation

---
+++ ms-user/src/main/java/az/edu/turing/msuser/service/ProfileService.java
@@ -0,0 +1,23 @@
+package az.edu.turing.msuser.service;
+
+
+import az.edu.turing.msuser.model.dto.ProfileDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+
+public interface ProfileService {
+
+    ProfileDto addProfile(Long userId, ProfileDto profileDto);
+
+    ProfileDto updateProfile(Long userId,Long profileId, ProfileDto profileDto);
+
+    ProfileDto getProfileById(Long userId,Long profileId);
+
+    void deleteProfileById(Long userId, Long profileId);
+
+    Page<ProfileDto> getAllProfiles(Long id,Pageable pageable);
+
+//    void deleteAllProfiles();
+
+}
