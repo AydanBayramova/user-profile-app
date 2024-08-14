fe033b6c56663739484892dbafffc879efcefbfe
AydanBayramova <bayramoffaaydan1@gmail.com>
2024-08-14 19:49:50 +0400

    change some functionality ProfileEntity

--- bff/src/main/java/az/edu/turing/bff/service/BffUserProfileService.java
+++ bff/src/main/java/az/edu/turing/bff/service/BffUserProfileService.java
@@ -3,9 +3,11 @@ package az.edu.turing.bff.service;
 import az.edu.turing.bff.dto.ProfileDto;
 import az.edu.turing.bff.client.UserProfileClient;
 
-
-import org.springframework.data.domain.PageImpl;
+import az.edu.turing.bff.dto.UserDto;
+import org.springdoc.core.converters.models.Pageable;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -18,9 +20,13 @@ public class BffUserProfileService {
         this.userProfileClient = userProfileClient;
     }
 
-    public ProfileDto getUserProfile(Long userId) {
+    public UserDto getUserProfile(Long userId) {
         return userProfileClient.getUserProfileById(userId);
     }
+
+  public ProfileDto getProfileId(Long userId,Long profileId) {
+        return userProfileClient.getProfileId(userId,profileId);
+  }
 
 
 }
