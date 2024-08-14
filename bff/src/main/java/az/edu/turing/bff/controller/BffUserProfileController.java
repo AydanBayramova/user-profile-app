1211ac297f4bae99628533ca074ed5ef8b0eaae0
AydanBayramova <bayramoffaaydan1@gmail.com>
2024-08-14 14:14:04 +0400

    BffUserProfileController class in bff module

---
+++ bff/src/main/java/az/edu/turing/bff/controller/BffUserProfileController.java
@@ -0,0 +1,29 @@
+package az.edu.turing.bff.controller;
+
+import az.edu.turing.bff.dto.ProfileDto;
+import az.edu.turing.bff.service.BffUserProfileService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/bff/users")
+public class BffUserProfileController {
+
+  private final BffUserProfileService userProfileService;
+
+    public BffUserProfileController(BffUserProfileService userProfileService) {
+        this.userProfileService = userProfileService;
+    }
+
+    @GetMapping("/{userId}/profiles")
+    public ProfileDto getUserProfiles(@PathVariable Long userId) {
+        return userProfileService.getUserProfile(userId);
+    }
+
+}
