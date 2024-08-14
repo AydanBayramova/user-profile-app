98db32723e5310024275bbbc722b7e93c3e61d53
AydanBayramova <bayramoffaaydan1@gmail.com>
2024-08-14 14:12:39 +0400

    ProfileRepository interface that extent spring data Jpa

---
+++ ms-user/src/main/java/az/edu/turing/msuser/domain/repository/ProfileRepository.java
@@ -0,0 +1,19 @@
+package az.edu.turing.msuser.domain.repository;
+
+
+import az.edu.turing.msuser.domain.entity.ProfileEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+@Repository
+public interface ProfileRepository extends JpaRepository<ProfileEntity, Long> {
+
+    Page<ProfileEntity> findAllByUserId(Long userId, Pageable pageable);
+
+    Optional<ProfileEntity>findByUserIdAndId(Long userId, Long profileId);
+}
