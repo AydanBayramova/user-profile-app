d769b509202f72695f3c453a7a85102479e82faa
AydanBayramova <bayramoffaaydan1@gmail.com>
2024-08-15 12:48:34 +0400

    comment anototions and validator

--- ms-user/src/main/java/az/edu/turing/msuser/annotation/EmailOrPhoneRequired.java
+++ ms-user/src/main/java/az/edu/turing/msuser/annotation/EmailOrPhoneRequired.java
@@ -1,20 +1,20 @@
-package az.edu.turing.msuser.annotation;
-
-import jakarta.validation.Constraint;
-import jakarta.validation.Payload;
-
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-
-@Target({ElementType.TYPE})
-@Retention(RetentionPolicy.RUNTIME)
-@Constraint(validatedBy = az.edu.turing.msuser.validators.EmailOrPhoneValidator.class)
-public @interface EmailOrPhoneRequired {
-    String message() default "{Either email or phone number must be provided}";
-
-    Class<?>[] groups() default {};
-
-    Class<? extends Payload>[] payload() default {};
-}
+//package az.edu.turing.msuser.annotation;
+//
+//import jakarta.validation.Constraint;
+//import jakarta.validation.Payload;
+//
+//import java.lang.annotation.ElementType;
+//import java.lang.annotation.Retention;
+//import java.lang.annotation.RetentionPolicy;
+//import java.lang.annotation.Target;
+//
+//@Target({ElementType.TYPE})
+//@Retention(RetentionPolicy.RUNTIME)
+//@Constraint(validatedBy = az.edu.turing.msuser.validators.EmailOrPhoneValidator.class)
+//public @interface EmailOrPhoneRequired {
+//    String message() default "{Either email or phone number must be provided}";
+//
+//    Class<?>[] groups() default {};
+//
+//    Class<? extends Payload>[] payload() default {};
+//}
