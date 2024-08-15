51df4b912475992bdab16cff67a77e4459a895a1
AydanBayramova <bayramoffaaydan1@gmail.com>
2024-08-15 12:48:34 +0400

    comment anototions and validator

--- ms-user/src/main/java/az/edu/turing/msuser/validators/EmailOrPhoneValidator.java
+++ ms-user/src/main/java/az/edu/turing/msuser/validators/EmailOrPhoneValidator.java
@@ -1,18 +1,32 @@
-package az.edu.turing.msuser.validators;
-
-import az.edu.turing.msuser.annotation.EmailOrPhoneRequired;
-import az.edu.turing.msuser.domain.entity.ProfileEntity;
-import jakarta.validation.ConstraintValidator;
-import jakarta.validation.ConstraintValidatorContext;
-
-public class EmailOrPhoneValidator implements ConstraintValidator<EmailOrPhoneRequired, ProfileEntity> {
-
-    @Override
-    public boolean isValid(ProfileEntity profileEntity, ConstraintValidatorContext constraintValidatorContext) {
-        if (profileEntity == null) {
-            return false;
-        }
-        return profileEntity.getEmail() != null && !profileEntity.getEmail().isEmpty()
-                || profileEntity.getPhoneNumber() != null && !profileEntity.getPhoneNumber().isEmpty();
-    }
-}
+//package az.edu.turing.msuser.validators;
+//
+//import az.edu.turing.msuser.annotation.EmailOrPhoneRequired;
+//import az.edu.turing.msuser.domain.entity.ProfileEntity;
+//import az.edu.turing.msuser.model.dto.ProfileDto;
+//import jakarta.validation.ConstraintValidator;
+//import jakarta.validation.ConstraintValidatorContext;
+//
+//public class EmailOrPhoneValidator implements ConstraintValidator<EmailOrPhoneRequired, ProfileDto> {
+//
+//    @Override
+//    public boolean isValid(ProfileDto profileDto, ConstraintValidatorContext context) {
+//        if (profileDto == null) {
+//            return false;
+//        }
+//        boolean hasEmail = profileDto.getEmail() != null && !profileDto.getEmail().trim().isEmpty();
+//        boolean hasPhoneNumber = profileDto.getPhoneNumber() != null && !profileDto.getPhoneNumber().trim().isEmpty();
+//
+//        if (!hasEmail && !hasPhoneNumber) {
+//            context.disableDefaultConstraintViolation();
+//            context.buildConstraintViolationWithTemplate("Either email or phone number must be provided.")
+//                    .addPropertyNode("email")
+//                    .addConstraintViolation();
+//            System.out.println("testgtygiudwvldvc");
+//            return false;
+//        }
+//        System.out.println("ygdiygdeid7e38d");
+//        return true; // Əgər biri doludursa, validator true qaytarır.
+//    }
+//
+//
+//}
